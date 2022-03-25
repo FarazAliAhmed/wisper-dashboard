3684ca746b93a1c2af18e456a87e739a14b74bc6
Simon <simonorugbo15@gmail.com>
2022-03-25 11:48:37 +0100

    added link to the support

--- src/components/dashboard/SupportCard.js
+++ src/components/dashboard/SupportCard.js
@@ -1,4 +1,5 @@
 import { Card, CardBody, CardTitle, Button, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 import "./../../assets/scss/custom.scss";
 
@@ -15,12 +16,28 @@ const SupportCard = () => {
             </p>
           </Col>
           <Col sm="12" md="4" lg="3">
-            <Button color="primary " className="btn-block">
-              Whatsapp Us
-            </Button>
-            <Button color="primary" className="mt-2 btn-block">
-              Join Our Whatsapp Group
-            </Button>
+            <Link
+              to={{
+                pathname: "https://chat.whatsapp.com/GKt0Eb5tvtK1mATZQKovIR",
+              }}
+              target="_blank"
+            >
+              <Button color="primary " className="btn-block">
+                Whatsapp Us
+              </Button>
+            </Link>
+
+            <Link
+              to={{
+                pathname: "https://wa.me/message/V5ZBZYUNYXSXC1",
+              }}
+              target="_blank"
+            >
+              {" "}
+              <Button color="primary" className="mt-2 btn-block">
+                Join Our Whatsapp Group
+              </Button>
+            </Link>
           </Col>
         </Row>
       </CardBody>
